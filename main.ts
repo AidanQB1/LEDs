a3611f2019bdfe069f4510139d8bea4cdb66ce74
AidanQB1 <92817224+AidanQB1@users.noreply.github.com>
2021-10-21 13:53:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . # #
+        . . # # .
+        . # # . .
+        # # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . # # . .
+        . . # # .
+        . . . # #
+        . . . . #
+        `)
+})
